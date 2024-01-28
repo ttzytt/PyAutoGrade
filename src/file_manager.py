41e2e9171cab1df685af1bfc9f05ee04e7188160
ttzytt <Zyt2006613@outlook.com>
2024-01-28 15:13:40 -0500

    complete code obfuscate

--- src/file_manager.py
+++ src/file_manager.py
@@ -7,10 +7,17 @@ import datetime
 import shutil
 from tqdm import tqdm
 import argparse
+import random
+import pathlib
+import re
+
 class FileManager(CfgFileRelated):
     def __init__(self, cfg_path : str = './config.yml') -> None:
         super().__init__(cfg_path)  
     
+    def get_timestr(self) -> str:
+        return str(datetime.datetime.now()).replace(':', '#')
+
     def get_all_by_problem(self, isfiles : list[bool], *p_or_folder_names : str):
         """ 
             Move all students' solution for problem/folder name into `temp_files_path`
@@ -27,8 +34,7 @@ class FileManager(CfgFileRelated):
                     ... 
                 ...
         """
-        timestr =  str(datetime.datetime.now())
-        timestr = timestr.replace(':', '#')
+        timestr = self.get_timestr()
         isfile_pfname_pbar = tqdm(zip(isfiles, p_or_folder_names))
         for isfile, pfname in isfile_pfname_pbar:
             isfile_pfname_pbar.set_description(f"Copying file/folder: {pfname}")
@@ -63,6 +69,56 @@ class FileManager(CfgFileRelated):
         """
         shutil.rmtree(self.temp_files_abs_path)
         os.mkdir(self.temp_files_abs_path)
+    
+    def obfuscate_tested_codes(self):
+        """
+            This function is designed to meet the compliance rule of the school. 
+            Specifically, it will replace the name of the student in the input folder 
+            with a random number. Then it will delet    e all comments in the solution file,
+            as in some cases, students indicate their name in comments.
+        
+        """
+        stus : list[str] = self.cfg['students_list']
+        stus_to_num : dict[str, int] = {}
+        random.shuffle(stus)
+        for num, stu in enumerate(stus):
+            stus_to_num[stu] = num
+        
+        def remove_comments(program : str) -> str:
+            # Remove single-line comments
+            program = re.sub(r'#.*?\n', '\n', program)
+            
+            # Remove multi-line comments
+            program = re.sub(r'""".*?"""', '', program, flags=re.DOTALL)
+            
+            return program
+        
+        copy_folder_name = "obfuscated_tested_codes [copied at {}]".format(self.get_timestr())
+
+        for stu in stus: 
+            stu_path = pathlib.Path(os.path.join(self.tested_code_abs_path, stu))
+            stu_num = stus_to_num[stu]
+            for pyfile_path in stu_path.rglob("*.py"):
+                with open(pyfile_path, 'r') as f:
+                    program = f.read()
+                program = remove_comments(program)
+                # replace the name of the student with a random number in pyfile_path
+                # also make sure that the new file is in the cache
+                
+                pyfile_name = str(pyfile_path).replace(get_abs_norm_path_str(self.tested_code_abs_path), '').replace(stu, "", 1)
+                # remove all / at the beginning of the path
+                while pyfile_name[0] == '/':
+                    pyfile_name = pyfile_name[1:]
+                no_name_path_with_file = os.path.join(self.cfg_folder_abs_path, self.temp_files_rel_path, copy_folder_name, str(stu_num), pyfile_name)
+                # create the folder if not exists
+                # remove the last part to make it become a folder
+                no_name_path = os.path.join(*Path(no_name_path_with_file).parts[:-1])
+                if not os.path.exists(no_name_path):
+                    os.makedirs(no_name_path)
+                with open(no_name_path_with_file, 'w') as f:
+                    f.write(program)
+            
+                
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Copy all students\' file or folder into a folder in `temp_files_path`')
@@ -72,6 +128,7 @@ if __name__ == "__main__":
     parser.add_argument('--move_files', '-mf', type=str, nargs='+', help='move files to `temp_files_path`')
     parser.add_argument('--move_directories', '-md', type=str, nargs='+', help='move folders to `temp_files_path`')
     parser.add_argument('--clear_cache', '-cc', action='store_true', help='clear all files in `temp_files_path`')
+    parser.add_argument('--obfuscate_tested_codes', '-oc', action='store_true', help='obfuscate all tested codes')
     args = parser.parse_args()
     fm = FileManager(args.cfg)
     if args.clear_cache:
@@ -80,4 +137,5 @@ if __name__ == "__main__":
         fm.get_all_by_problem([True] * len(args.move_files), *args.move_files)
     if args.move_directories:
         fm.get_all_by_problem([False] * len(args.move_directories), *args.move_directories)
-        
+    if args.obfuscate_tested_codes:
+        fm.obfuscate_tested_codes()
--- src/helpers.py
+++ src/helpers.py
@@ -21,6 +21,11 @@ def replace_first_path_occurance(path : str, old : str, new : str):
             break
     return os.path.join(*parts)
 
+def get_abs_norm_path_str(path : str) -> str:
+    return str(Path(path).resolve())
+def get_norm_path_str(path : str) -> str:
+    return str(Path(path))
+
 class CfgFileRelated: 
     def _get_abs_path_in_cfg(self, path: str) -> str:
         # get the absolute path of `path` in the cfg file
@@ -36,11 +41,15 @@ class CfgFileRelated:
             *Path(self.cfg_abspath).parts[:-1])
         self.tested_code_abs_path = self._get_abs_path_in_cfg(
             self.cfg['tested_code_path'])
+        self.tested_code_rel_path = self.cfg['tested_code_path']
         self.test_case_abs_path = self._get_abs_path_in_cfg(
             self.cfg['test_case_path'])
+        self.test_case_rel_path = self.cfg['test_case_path']
         self.temp_files_abs_path = self._get_abs_path_in_cfg(
             self.cfg['temp_files_path']
         )
+        self.temp_files_rel_path = self.cfg['temp_files_path']
         self.output_abs_path = self._get_abs_path_in_cfg(
             self.cfg['output_path']
         )
+        self.output_rel_path = self.cfg['output_path']
